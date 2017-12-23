bf7f34a3e8e3242450e4f361a9ab7ac441e567df
VladLapshin <34584139+VladLapshin@users.noreply.github.com>
2017-12-23 12:46:25 +0300

    Vlad23.12.2017

--- Project/Form1.cs
+++ Project/Form1.cs
@@ -29,7 +29,7 @@ namespace WindowsFormsApp1
             int[] x = new int[2];
             x = Ivan.SelectedTextIntoIndexForSemanticFragmentTable(textBox1);
             Ivan.AddIndexIntoSemanticFragmentTable("C://Users/ivan_/Desktop/time.txt");
-
+            //Vlad.b1(listBox1);
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -44,7 +44,7 @@ namespace WindowsFormsApp1
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            //Vlad.lb1(listBox1, textBox1);
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
---
+++ VladLibrary/ClassLibrary1/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ClassLibrary1
+{
+    public class Vlad
+    {
+            string tmp;
+            string t;
+            string sf = "СФ";
+            int sfchi = 0;
+            int tl;
+            int ts;
+            int i;
+            //(@"C:\Users\Влад\Desktop\Влад сем9\Нетрогать_Влад\СФы.txt")
+            List<string> rlt = new List<string>();
+            
+            //fhhjb
+ 
+            public void b1(System.Windows.Forms.ListBox listBox1)
+            {
+                using (StreamReader rdr = new StreamReader(@"C:\Users\Влад\Desktop\Влад сем9\Нетрогать_Влад\СФы.txt"))
+                {
+                    do
+                    {
+                        tmp = rdr.ReadLine();
+                        if (tmp == null) break;
+                        rlt.Add(tmp);
+                        sfchi = sfchi + 1;
+                        listBox1.Items.Add(sf + Convert.ToString(sfchi));
+                    } while (true);
+                }
+            }
+
+            private void lb1(System.Windows.Forms.ListBox listBox1,TextBox textBox1)
+            {
+                i = listBox1.SelectedIndex;
+                t = Convert.ToString(rlt.ElementAt(i));
+                string[] t1 = t.Split('\t');
+                ts = Convert.ToInt32(t1[1]);
+                tl = Convert.ToInt32(t1[2]);
+                textBox1.Select(ts, tl);
+            }
+     }
+
+}
